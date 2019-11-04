a7dce8e65fa78d1bfc6bf504a43abd24419bced7
Kirana <kirana.mr@gigitalshark.in>
2019-11-04 19:51:23 +0530

    images added

--- src/components/home-component.js
+++ src/components/home-component.js
@@ -3,6 +3,352 @@ export default class Home extends Component {
   render() {
     return (
       <div>
+        <div className="col-md-12">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (1).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (2).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (3).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (4).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (5).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (6).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (7).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (8).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (9).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (10).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (11).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (12).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (13).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (14).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (15).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (16).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (17).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (18).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (19).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (20).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (21).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (22).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (23).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (24).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (25).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (26).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (27).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (28).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (29).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (30).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (31).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (32).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (33).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (34).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (35).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (36).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (37).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (38).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (39).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (40).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (41).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (42).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (43).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-3" style={{ marginTop: "20px" }}>
+                <img
+                  src={require("../images/1 (44).jpg")}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
         <h5>
           We are co-existed with the contemporary applied sciences and
           engineering mechanics.
